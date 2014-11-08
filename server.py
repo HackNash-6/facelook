035491e244990cf6341bb91bd7bcbe03d874b2b3
mahesh.thundathil <mahesh.thundathil@digitalreasoning.com>
2014-11-08 15:57:05 -0600

    adding cors

--- server.py
+++ server.py
@@ -5,10 +5,24 @@ from naive_similarity import compare_similarity
 import os
 import tempfile
 
+try:
+    from flask.ext.cors import CORS # The typical way to import flask-cors
+    from flask.ext.cors import cross_origin
+except ImportError:
+    # Path hack allows examples to be run without installation.
+    import os
+    parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.sys.path.insert(0, parentdir)
+    from flask.ext.cors import CORS
+
+
 app = Flask(__name__)
+# One of the simplest configurations. Exposes all resources matching /api/* to
+# CORS and allows the Content-Type header, which is necessary to POST JSON
+# cross origin.
+CORS(app, resources=r'/api/*', headers='Content-Type')
 
-
-@app.route('/similarity', methods=['POST'])
+@app.route('/api/similarity', methods=['POST'])
 def find_similar_images():
     prefix = request.url_root + 'images/'
     if request.headers['Content-Type'] == 'application/octet-stream':
@@ -22,7 +36,7 @@ def find_similar_images():
         return "415 Unsupported Media Type ;)"
 
 
-@app.route('/images/<image_name>')
+@app.route('/api/images/<image_name>')
 def lookup_image(image_name=None):
     return flask.send_file('images/' + image_name, mimetype='image/jpeg')
 
