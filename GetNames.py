6a8b16042bf362d2f0ddb5840b2c88542678a6b4
Jack <jackmalpo@gmail.com>
2014-11-08 12:14:55 -0600

    Stuff

--- GetNames.py
+++ GetNames.py
@@ -1,9 +1,29 @@
 __author__ = 'Jack'
+
 import requests
+from lxml import html
+celeb_names = []
 
-def getNames():
+def getNames(input_array):
+    #gets names from people.com
+
     allNames = requests.get('http://www.people.com/people/celebrities/')
-    html = allNames.text
-    print(html)
+    tree = html.fromstring(allNames.text)
 
-getNames()
+    #parse doc for names of celebs
+    celeb_elements = tree.xpath('//dt[. = "A"]/following-sibling::dd')
+    celebs_second = tree.xpath('//dt[. = "G"]/following-sibling::dd')
+    celebs_third = tree.xpath('//dt[. = "L"]/following-sibling::dd')
+    celebs_fourth = tree.xpath('//dt[. = "R"]/following-sibling::dd')
+
+    celeb_elements += celebs_second + celebs_third + celebs_fourth
+
+
+    for stuff in celeb_elements:
+        input_array.append(stuff.text_content())
+
+    print(input_array)
+
+
+
+
